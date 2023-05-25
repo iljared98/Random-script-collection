882c478578e5b0a84c37e10ec7dfee6c3c79705c
iljared98 <52998588+iljared98@users.noreply.github.com>
2023-05-25 15:39:52 -0500

    Update staffCreation.ps1

--- cps-scripts/active-directory/staff-accounts/staffCreation.ps1
+++ cps-scripts/active-directory/staff-accounts/staffCreation.ps1
@@ -3,17 +3,15 @@
 # Purpose      : Automating staff account creation and updating fields if they exist.
 
 # Change this first variable should the export file need to move for whatever reason.
-$MAS_EXPORT_FILE = "MAS_EXPORT.csv"
-# TODO: Robust logging for account creation.
-# Right now nothing is even being written to this file, maybe add the print statements later along with
-# timestamps???
-$LOGGING_FILE = "[PATH]\STAFF_CREATION.log" 
+#$MAS_EXPORT_FILE = "MAS_EXPORT.csv"
+$MAS_EXPORT_FILE = "MAS_EXPORT_TESTING.csv"
 
-# These function similarly to Jooel's AddUser.ini OU mappings
-# per site. The intention with District mapping to NULL is to ensure
-# District employees are being mapped to OUs by their job titles
-# by Michael's request.
+# Alternatively, execute these commands as administrator with a scheduled task : 
+# powershell ".\staffCreation.ps1" > staffCreation.log
+#$LOGGING_FILE = "[PATH]\STAFF_CREATION.log"
 
+#! All of these sites should conform to the two-character name standard soon,
+#! just waiting for the greenlight form Michael to change the container names in AD.
 $locationToOU = @{
     "Band" = "OU=Users,OU=Band,OU=STAFF,DC=cowetaps,DC=com"
     "CE" = "OU=Users,OU=CE,OU=STAFF,DC=cowetaps,DC=com"
@@ -36,79 +34,154 @@ $locationToOU = @{
 $usersToCreate = Import-CSV $MAS_EXPORT_FILE
 
 # This is an array of users to exclude from the update logic branch. Mostly includes VIPs such
-# as superintendents that may have multiple roles (i.e. Max Myers as district super / bus driver).
-# Please document who these employeeIDs belong to, as the employeeID is the main comparison field
-# for account duplication / field updates. 
-# 80314 -> Max Myers (district super)
-# Number -> User (role) etc...
-$employeeIDsToExclude = "80314", "1234567890"
+# as superintendents that may have multiple roles or for people who would complain enough if
+# their account switched to their legal name from whatever name they use now (i.e. Brad -> Kevin by 
+# the script's default control flow.). 
 
+# 80314 -> Max Myers (district super)
+# 80367 -> Brad Tackett (CF-NO)
+$employeeIDsToExclude = @("80314", "80367", "12345")
+
+Write-Host "            ################################################`
+            #    COWETA PUBLIC SCHOOLS STAFF ACCOUNT       #`
+            #                  GENERATOR                   #`
+            #                                              #`
+            ################################################"
+
+Write-Host "`nDATE: $(Get-Date -UFormat "%m/%d/%Y %R")`n"
+
+# TODO: Make Python script to prep Michael's MAS export file 
 # Main logic for staff account creation.
 foreach ($user in $usersToCreate) {
 
+    # Placeholders to remove annoying special characters; only removing ' and - from names for now.
+    $firstHolder = $user.firstName
+    $lastHolder = $user.lastName
+
     # Aligning values from CSV columns to variables..
     $EmployeeID = $user.employeeID
-    $FirstName = $user.firstName
-    $LastName = $user.lastName
-    $Username = $user.username 
+
+    # Stripping any apostrophes / dashes / whitespace from names.
+    $FirstName = $($firstHolder -replace "'","" -replace "-","" -replace " ","")
+    $LastName = $($lastHolder -replace "'","" -replace "-","" -replace " ","")
+
+    #$Username = $user.username 
+    $Username = "$FirstName.$LastName"
+
     $Password = $user.password
     $EmailAddress = $user.emailAddress
     $EmployeeType = $user.employeeType
     $JobTitle = $user.jobTitle
     $PrimaryLocation = $user.primaryLocation
-    # Only add this back into both the CSV and the script if Michael requests it. Otherwise keep commented out.
-    #$randomDigitColumn = $user.randomDigit # that one 2-3 digit column Michael put in the CSV. Forgot what it's for.
     $Company = $user.company
 
+    # TODO: Import phone numbers somehow.
+    #$PhoneExtension = $user.phoneNumber
+
     # Checks for duplicate users, using the employeeID field. If they already exist, update fields.
-    if (Get-ADUser -F {EmployeeID -eq $EmployeeID}) {
+    if ((Get-ADUser -F "employeeID -eq '$EmployeeID'" -Properties * | Select-Object -ExpandProperty EmployeeID)) {
 
         # Checks if the employeeID is currently in the exclusion array.
         if ($EmployeeID -in $employeeIDsToExclude) {
-            Write-Host "`nEmployee $Username is in the excluded accounts array, not updating attribute fields...`n"
+            Write-Host "`nEmployee $FirstName $LastName ($EmployeeID) is in the excluded accounts array, skipping field updates...`n"
         }
 
-        # Updating fields that would be pertinent. Might leave out the name change / email fields
-        # just in case that would cause errors on our cloud tenant side of things. HOWEVER, with how
-        # often people get remarried / divorced and it changes their name, may as well have it here just
-        # in case.
+        # Updating all pertinent fields.
         else {
-            Write-Host "`nEmployee ID exists and not excluded, updating user fields:`n"
-            Set-ADUser `
-            -Name $("$FirstName $LastName") `
-            -GivenName $FirstName `
-            -Path $userOUtoAssign `
-            -Surname $LastName `
-            -UserPrincipalName $("$Username@cowetaps.com") `
-            -DisplayName $("$FirstName $LastName") `
-			-Department $PrimaryLocation `
-            -Title $JobTitle `
-            -EmailAddress $EmailAddress 
 
+            $userObj = Get-ADUser -F "employeeID -eq '$EmployeeID'" -Properties * | Select-Object -ExpandProperty SamAccountName
+            $distName = Get-ADUser -F "employeeID -eq '$EmployeeID'" -Properties * | Select-Object -ExpandProperty DistinguishedName
 
+            # Doing this for updating fields of people with duplicate names.
+            # It's stupid but this is the easiest way I can think of, fix it if there's
+            # a better way (or if I figure something out). Compares if last 3 chars of the username
+            # equals first initial/last initial/employeeID (since it's appended to duplicate name users).
+            $truncatedNameHolder = $($userObj -replace '^.*(?=.{3}$)').ToString()
+            $appendedStringForDuplicates = $FirstName.Substring(0, 1) + $LastName.Substring(0, 1) + $EmployeeID.Substring($EmployeeID.get_Length() - 1)
+
+            #Write-Host "$truncatedNameHolder $appendedStringForDuplicates"
+
+            if ($truncatedNameHolder -eq $appendedStringForDuplicates) {
+                Write-Host "$userObj was initially created with a duplicate name, keeping former naming convention.."
+                $Username = $Username + $appendedStringForDuplicates
+
+                $userOUtoAssign = $locationToOU[$PrimaryLocation]
+
+                $LastName = $LastName + $FirstName.Substring(0, 1) + $LastName.Substring(0, 1) + $EmployeeID.Substring($EmployeeID.get_Length() - 1)
+
+                Set-ADUser `
+                -Identity $userObj `
+                -GivenName $FirstName `
+                -Surname $LastName `
+                -UserPrincipalName $("$Username@cowetaps.com") `
+                -SamAccountName $Username `
+                -Description $EmployeeType `
+                -DisplayName $("$FirstName $LastName") `
+                -Department $PrimaryLocation `
+                -Title $JobTitle `
+                -EmailAddress $("$Username@cowetaps.com") `
+                -Company $Company 
+
+                Move-ADObject -Identity $distName -TargetPath $userOUtoAssign
+                Rename-ADObject -Identity $distName -NewName $("$FirstName $LastName")
+
+                # FIXME: Format better with backticks.
+                Write-Host "UPDATING ACCOUNT : $Username`n`First Name: $FirstName`n`Last Name: $LastName`n`Temp Password: $Password`n`Email: $EmailAddress`n`Employee Type: $EmployeeType`n`Job Title: $JobTitle`n`Primary Location: $PrimaryLocation`n`Company: $Company`n`Employee ID: $EmployeeID`n"
+                Write-Host "Moving $userObj to : $userOUtoAssign`n"
+
+            }
+
+            else {
+                $userOUtoAssign = $locationToOU[$PrimaryLocation]
+
+                Set-ADUser `
+                -Identity $userObj `
+                -GivenName $FirstName `
+                -Surname $LastName `
+                -UserPrincipalName $("$Username@cowetaps.com") `
+                -SamAccountName $Username `
+                -Description $EmployeeType `
+                -DisplayName $("$FirstName $LastName") `
+                -Department $PrimaryLocation `
+                -Title $JobTitle `
+                -EmailAddress $EmailAddress `
+                -Company $Company 
+
+                Move-ADObject -Identity $distName -TargetPath $userOUtoAssign
+                Rename-ADObject -Identity $distName -NewName $("$FirstName $LastName")
+
+                # TODO : Fix backticks not working on this correctly for some reason.
+                Write-Host "UPDATING ACCOUNT : $Username`n`First Name: $FirstName`n`Last Name: $LastName`n`Temp Password: $Password`n`Email: $EmailAddress`n`Employee Type: $EmployeeType`n`Job Title: $JobTitle`n`Primary Location: $PrimaryLocation`n`Company: $Company`n`Employee ID: $EmployeeID`n"
+                Write-Host "Moving $userObj to : $userOUtoAssign`n"
+            }
         }
     }
 
     # Main magic that happens.
     else {
-
-        # TODO: Add logic branch here to fix duplicate names.
-        # Probably use UPNs since we didn't make SAMs for those mass generated accounts.
-        if (Get-ADUser ) {
-            Write-Host "Duplicate name $Username exists but no matching employeeID found, making new user with digit appended to name."
-            
-        }
         $userOUtoAssign = $locationToOU[$PrimaryLocation]
-        # Currently Description maps to the variable $EmployeeType (certified/support). Change if we add a custom extended field later.
         
-        # For the time being, Description is being hijacked by : EMPLOYEE TYPE, or for students, [information that would be useful].
+        # My workaround for the rare instance of a duplicate employee name.
+        # [ first initial ][ last initial ][ last digit of employee ID ] convention.
+        # I went with this method to deal with duplicate names because 2 random digits would be too inconsistent and unreliable to deal with.
+        # This should (in a perfect world) handle up to 10 staff with a duplicate name, as 0-9 will be the last digit of the ID number.
+        if ((Get-ADUser -F "SamAccountName -eq '$Username'" -Properties * | Select-Object -ExpandProperty SamAccountName)) {
+            Write-Host "Username combination $Username already exists, appending initials and last employee ID digit for UPN/SAM: `n"
+            $Username = $Username + $FirstName.Substring(0, 1) + $LastName.Substring(0, 1) + $EmployeeID.Substring($EmployeeID.get_Length() - 1)
+            Write-Host "New Username: $Username`n"
+            $LastName = $LastName + $FirstName.Substring(0, 1) + $LastName.Substring(0, 1) + $EmployeeID.Substring($EmployeeID.get_Length() - 1)
+        }
+
+        # For the time being, Description is being hijacked by : EMPLOYEE TYPE
         New-ADUser `
             -Name $("$FirstName $LastName") `
             -GivenName $FirstName `
             -Path $userOUtoAssign `
             -Surname $LastName `
             -UserPrincipalName $("$Username@cowetaps.com") `
+            -SamAccountName $Username `
             -EmployeeID $EmployeeID `
+            -Description $EmployeeType `
             -DisplayName $("$FirstName $LastName") `
 			-Department $PrimaryLocation `
             -Title $JobTitle `
@@ -118,35 +191,8 @@ foreach ($user in $usersToCreate) {
             -ChangePasswordAtLogon $True `
             -Enabled $True
 
-        Write-Host "CREATING ACCOUNT : $Username. `nFirstName: $FirstName`nLastName: $LastName`nPass: $Password`nEmail: $EmailAddress`nEmployeeType: $EmployeeType`nJobTitle: $JobTitle`nPrimaryLocation: $PrimaryLocation`nCompany: $Company`nEmpID: $EmployeeID`n"
+        # FIXME: Format this better with backticks.
+        Write-Host "CREATING ACCOUNT : $Username`n`First Name: $FirstName`n`Last Name: $LastName`n`Password: $Password`n`Email: $EmailAddress`n`Employee Type: $EmployeeType`n`Job Title: $JobTitle`n`Primary Location: $PrimaryLocation`n`Company: $Company`n`Employee ID: $EmployeeID`n"
         Write-Host "$userOUtoAssign is the OU for this account.`n`n"
-        
     }
 }
-
-# Put this block back on lines 34-58 IF we need it, otherwise just leave it here.
-
-# Also, create distinctions for generic role such as SECRETARY in case it is repeated at multiple sites (i.e. a maintenance or 
-# transportation secretary).
-# ADD DISTRICT TITLES / RESPECTIVE OUs TO THIS DICTIONARY AS NECESSARY. OTHER SITES DO NOT NEED THIS.
-# This is due to the definition of "District" employees being very broad in MAS exports.
-<# $titleToOU = @{
-    "ASST GROUNDS MANAGER" = "OU=District,OU=STAFF,DC=cowetaps,DC=com"
-    "DISTRICT NURSE" = "OU=District,OU=STAFF,DC=cowetaps,DC=com"
-    "Nurse" = "OU=District,OU=STAFF,DC=cowetaps,DC=com" # Set as is to match what is in the CSV. Why we can't just make this DISTRICT NURSE is beyond me.
-    "LAY COACH" = "OU=District,OU=STAFF,DC=cowetaps,DC=com"
-    "SECRETARY" = "" # TODO: There is a definition for this both as a Maintenance and District employee. Need to figure something out.
-    "DIRECTOR CHILD NUTRITION" = "OU=Child Nutrition,OU=STAFF,DC=cowetaps,DC=com"
-    "DIRECTOR MAINTENANCE" = "OU=Maintenance,OU=STAFF,DC=cowetaps,DC=com"
-    "MAINTENANCE" = "OU=Maintenance,OU=STAFF,DC=cowetaps,DC=com"
-
-    # Why can't we just have straightforward job titles...
-    "BUS AND SHUTTLE DRIVER-FT" = "OU=Transportation,OU=STAFF,DC=cowetaps,DC=com"
-    "BUS ASSISTANT" = "OU=Transportation,OU=STAFF,DC=cowetaps,DC=com"
-    "BUS DRIVER" = "OU=Transportation,OU=STAFF,DC=cowetaps,DC=com"
-    "BUS DRIVER AND SHUTTLE" = "OU=Transportation,OU=STAFF,DC=cowetaps,DC=com"
-    "BUS DRIVER/DELIVERY DRIVER" = "OU=Transportation,OU=STAFF,DC=cowetaps,DC=com"
-    "DIRECTOR TRANSPORTATION" = "OU=Transportation,OU=STAFF,DC=cowetaps,DC=com"
-    "SHUTTLE DRIVER"= "OU=Transportation,OU=STAFF,DC=cowetaps,DC=com"
-    "HEAD MECHANIC" = "OU=Transportation,OU=STAFF,DC=cowetaps,DC=com"
-} #>
